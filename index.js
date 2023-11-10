58efa21cd01f07559fa5ff18de9a85cf299d44e0
João Silva <joao.silva@uphold.com>
2023-11-10 23:32:13 +0000

    support: implement connected callback and simplify slotted content[C

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 class WithVisibility extends HTMLElement {
-  constructor({threshold = 1, addToParent = true} = {}) {
+  constructor({threshold = 1} = {}) {
     super();
 
     const observer = new IntersectionObserver((observers) => {
-      const actualObserver = observers[0];
+        const actualObserver = observers[0];
 
-      if (actualObserver.isIntersecting) {
-        if (!this.classList.contains('visible')) {
-          this.classList.add('visible');
-        }
+        if (actualObserver.isIntersecting) {
+          if (!this.classList.contains('visible')) {
+            this.classList.add('visible');
+          }
 
-        if (this.parentElement && !this.parentElement.classList.contains('visible')) {
-          this.parentElement.classList.add('visible')
-        }
-      } else {
-        if (this.classList.contains('visible')) {
-          this.classList.remove('visible');
-        }
+          if (this.parentElement && !this.parentElement.classList.contains('visible')) {
+            this.parentElement.classList.add('visible')
+          }
+        } else {
+          if (this.classList.contains('visible')) {
+            this.classList.remove('visible');
+          }
 
-        if (
-            this.parentElement && !this.nextElementSibling && actualObserver.boundingClientRect.bottom < window.innerHeight) {
-          this.parentElement.classList.remove('visible');
+          if (
+              this.parentElement && !this.nextElementSibling && actualObserver.boundingClientRect.bottom < window.innerHeight) {
+            this.parentElement.classList.remove('visible');
+          }
         }
-      }
-    }, { threshold: threshold})
+      }, { threshold: threshold})
 
-    observer.observe(this);
+      observer.observe(this);
   }
 }
 
 class BlogPost extends WithVisibility {
   constructor() {
     super();
+  }
 
+  connectedCallback() {
     this.classList.add('blogPost')
   }
 }
@@ -40,95 +42,109 @@ class BlogPost extends WithVisibility {
 class BlogImage extends WithVisibility {
   constructor() {
     super({threshold: 0.2});
-
-    ['src', 'alt'].map((attr) => {
-      if (!this.hasAttribute(attr)) {
-        throw new Error(`<blog-image> component requires '${attr}' attribute`);
-      }
-    })
-
-    const description = this.getAttribute('alt');
-    const source = this.getAttribute('src');
-
-    this.classList.add('blogImage');
-
-    this.innerHTML = `
-    <a target="_blank" rel="noopener noreferrer" href="${source}"><img alt="${description}" src="${source}" /></a>
-    `;
   }
+
+  connectedCallback() {
+    ['src', 'alt'].map((attr) => {
+        if (!this.hasAttribute(attr)) {
+          throw new Error(`<blog-image> component requires '${attr}' attribute`);
+        }
+      })
+
+      this.classList.add('blogImage');
+
+      this.innerHTML = `
+      <a target="_blank" rel="noopener noreferrer" href="${this.getAttribute('src')}"><img alt="${this.getAttribute('alt')}" src="${this.getAttribute('src')}" /></a>
+      `;
+    }
 }
 
 class BlogVideo extends WithVisibility {
   constructor() {
     super({threshold: 0.2});
-
-    ['src'].map((attr) => {
-      if (!this.hasAttribute(attr)) {
-        throw new Error(`<blog-image> component requires '${attr}' attribute`);
-      }
-    })
-
-    const source = this.getAttribute('src');
-
-    this.classList.add('blogVideo');
-
-    this.innerHTML = `
-    <video playsinline controls preload="metadata">
-      <source src="${source}" type="video/quicktime">
-    </video>
-
-    `;
   }
+
+  connectedCallback() {
+    ['src'].map((attr) => {
+        if (!this.hasAttribute(attr)) {
+          throw new Error(`<blog-image> component requires '${attr}' attribute`);
+        }
+      })
+
+      const source = this.getAttribute('src');
+
+      this.classList.add('blogVideo');
+
+      this.innerHTML = `
+      <video playsinline controls preload="metadata">
+        <source src="${source}" type="video/quicktime">
+      </video>
+      `;
+    }
 }
 
 class BlogList extends HTMLElement {
+  static slotted;
+
   constructor() {
     super();
 
+    this.slotted = this.innerHTML;
+  }
+
+  connectedCallback() {
     this.classList.add('blogListContainer')
 
     this.innerHTML = `
       <div class="container" aria-label="${this.getAttribute('emoji') ?? '🧑‍💻'}">
-      ${this.innerHTML}
+      ${this.slotted}
       </div>
-      
+
       <span aria-hidden="true">${this.getAttribute('emoji') ?? '🧑‍💻'}</span>
     `;
   }
 }
 
 class HelpDialog extends HTMLElement {
+  static slotted;
+
   constructor() {
     super();
 
-    this.classList.add('helpDialog');
+    this.slotted = this.innerHTML;
+  }
 
+  connectedCallback() {
     this.innerHTML = `
     <dialog id="modal">
       <div class="container">
-        ${this.innerHTML}
+        ${this.slotted}
         <form method="dialog">
           <button type="submit">Close</button>
         </form>
       </div>
     </dialog>
     `
+
+    this.firstElementChild.classList.add('helpDialog');
   }
 }
 
 class HelpButton extends HTMLElement {
   constructor() {
     super();
+  }
 
-    this.classList.add('helpButton');
+  connectedCallback() {
+    const button = document.createElement('button');
 
-    this.innerHTML = `<button>?</button>`;
-
-    this.firstChild.onclick = () => {
-      console.log('reached click');
-
+    button.textContent = "🤔"
+    button.classList.add('helpButton');
+    button.onclick = () => {
       document.getElementById('modal').showModal()
     };
+
+    this.appendChild(button);
   }
 }
 
